abc68cec58c9420069c16a7251b94ec66322cbc1
hallymer <mu072468@gmail.com>
2021-01-04 01:19:52 +0900

    add 30분 요약시리즈 - JS 2부 [2021.01.04]
    021의 user 데이터

---
+++ JavaScript/user.js
@@ -0,0 +1,182 @@
+var user = [
+  {
+    "_id": "5ff1edbb598aa0116a47735e",
+    "age": 39,
+    "eyeColor": "blue",
+    "name": "Strickland Bradford",
+    "gender": "male",
+    "email": "stricklandbradford@dragbot.com",
+    "phone": "+1 (848) 410-2170"
+  },
+  {
+    "_id": "5ff1edbb04e39708e6d0d5f4",
+    "age": 40,
+    "eyeColor": "brown",
+    "name": "Haynes Perez",
+    "gender": "male",
+    "email": "haynesperez@dragbot.com",
+    "phone": "+1 (977) 583-3480"
+  },
+  {
+    "_id": "5ff1edbb9c69a23392483576",
+    "age": 36,
+    "eyeColor": "blue",
+    "name": "Tamika Houston",
+    "gender": "female",
+    "email": "tamikahouston@dragbot.com",
+    "phone": "+1 (944) 576-2648"
+  },
+  {
+    "_id": "5ff1edbb84930faccc5074bd",
+    "age": 29,
+    "eyeColor": "brown",
+    "name": "Hess Glover",
+    "gender": "male",
+    "email": "hessglover@dragbot.com",
+    "phone": "+1 (851) 562-2857"
+  },
+  {
+    "_id": "5ff1edbb87b695ad76962a69",
+    "age": 23,
+    "eyeColor": "brown",
+    "name": "Pratt Hopkins",
+    "gender": "male",
+    "email": "pratthopkins@dragbot.com",
+    "phone": "+1 (863) 479-3477"
+  },
+  {
+    "_id": "5ff1edbb7d69234f63e5f538",
+    "age": 21,
+    "eyeColor": "brown",
+    "name": "Amy Battle",
+    "gender": "female",
+    "email": "amybattle@dragbot.com",
+    "phone": "+1 (912) 408-2492"
+  },
+  {
+    "_id": "5ff1edbb7d353a506c3bda82",
+    "age": 22,
+    "eyeColor": "blue",
+    "name": "Hernandez Kirby",
+    "gender": "male",
+    "email": "hernandezkirby@dragbot.com",
+    "phone": "+1 (825) 532-3398"
+  },
+  {
+    "_id": "5ff1edbb78e9c7837b492dbe",
+    "age": 35,
+    "eyeColor": "green",
+    "name": "Gentry Gallegos",
+    "gender": "male",
+    "email": "gentrygallegos@dragbot.com",
+    "phone": "+1 (945) 484-3858"
+  },
+  {
+    "_id": "5ff1edbb54adb200a097cb78",
+    "age": 39,
+    "eyeColor": "brown",
+    "name": "Matthews Harper",
+    "gender": "male",
+    "email": "matthewsharper@dragbot.com",
+    "phone": "+1 (851) 587-3546"
+  },
+  {
+    "_id": "5ff1edbb2e03ba73a1f363a4",
+    "age": 27,
+    "eyeColor": "blue",
+    "name": "Powell Stark",
+    "gender": "male",
+    "email": "powellstark@dragbot.com",
+    "phone": "+1 (986) 558-3180"
+  },
+  {
+    "_id": "5ff1edbb79887438d524f7ef",
+    "age": 27,
+    "eyeColor": "brown",
+    "name": "Rebekah Ellison",
+    "gender": "female",
+    "email": "rebekahellison@dragbot.com",
+    "phone": "+1 (848) 511-2671"
+  },
+  {
+    "_id": "5ff1edbbeefb32a53aa50dd2",
+    "age": 37,
+    "eyeColor": "blue",
+    "name": "Shana Rivers",
+    "gender": "female",
+    "email": "shanarivers@dragbot.com",
+    "phone": "+1 (915) 455-2620"
+  },
+  {
+    "_id": "5ff1edbb1a40e84af468798b",
+    "age": 25,
+    "eyeColor": "green",
+    "name": "Blake Merritt",
+    "gender": "male",
+    "email": "blakemerritt@dragbot.com",
+    "phone": "+1 (887) 409-2150"
+  },
+  {
+    "_id": "5ff1edbbe1f6b59f1c2c56fd",
+    "age": 30,
+    "eyeColor": "green",
+    "name": "Howard Parsons",
+    "gender": "male",
+    "email": "howardparsons@dragbot.com",
+    "phone": "+1 (850) 452-3944"
+  },
+  {
+    "_id": "5ff1edbbbc162e31496c824a",
+    "age": 29,
+    "eyeColor": "brown",
+    "name": "Laura Franklin",
+    "gender": "female",
+    "email": "laurafranklin@dragbot.com",
+    "phone": "+1 (821) 485-2627"
+  },
+  {
+    "_id": "5ff1edbb9255e75541a1d7b7",
+    "age": 34,
+    "eyeColor": "brown",
+    "name": "Judith Dixon",
+    "gender": "female",
+    "email": "judithdixon@dragbot.com",
+    "phone": "+1 (927) 536-3189"
+  },
+  {
+    "_id": "5ff1edbb68c09087eb578ee9",
+    "age": 22,
+    "eyeColor": "brown",
+    "name": "Bernard Randall",
+    "gender": "male",
+    "email": "bernardrandall@dragbot.com",
+    "phone": "+1 (994) 536-3798"
+  },
+  {
+    "_id": "5ff1edbb43c57b41cb837e71",
+    "age": 30,
+    "eyeColor": "blue",
+    "name": "Annabelle Kaufman",
+    "gender": "female",
+    "email": "annabellekaufman@dragbot.com",
+    "phone": "+1 (846) 591-3688"
+  },
+  {
+    "_id": "5ff1edbb7ddac70972da75a8",
+    "age": 23,
+    "eyeColor": "blue",
+    "name": "Mckee Deleon",
+    "gender": "male",
+    "email": "mckeedeleon@dragbot.com",
+    "phone": "+1 (952) 553-2093"
+  },
+  {
+    "_id": "5ff1edbbcd80f5260dbcb70b",
+    "age": 31,
+    "eyeColor": "blue",
+    "name": "Stokes Holman",
+    "gender": "male",
+    "email": "stokesholman@dragbot.com",
+    "phone": "+1 (943) 426-3026"
+  }
+]
